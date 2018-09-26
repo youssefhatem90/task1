17fe8e4c6364b027a19b4b1d8412049e7643b4c2
root <root@youssefhatem2.mylabserver.com>
2018-09-26 14:05:43 +0000

    ya raab

---
+++ app1/src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+import spark.servlet.SparkApplication;
+
+import static spark.Spark.get;
+
+public class HelloWorld implements SparkApplication {
+	public static void main(String[] args) {
+		new HelloWorld().init();
+	}
+
+	@Override
+	public void init() {
+
+		get("/", (req, res) -> "app1");
+	}
+}
